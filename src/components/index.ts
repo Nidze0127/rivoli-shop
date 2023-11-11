fadd9a5311a70365cd29f97a0d30539e7fd9253b
Nidzmens <abvg58@gmail.com>
2023-11-11 21:52:12 +0600

    feat: about us tiles component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import HeaderComponent from "./HeaderComponent.vue";
 import SwiperCarousel from "./SwiperCarousel.vue";
+import AboutUsTiles from "./AboutUsTiles.vue";
 
-export { HeaderComponent, SwiperCarousel };
+export { HeaderComponent, SwiperCarousel, AboutUsTiles };
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <template>
-  <main class="flex flex-col">
+  <main class="flex flex-col gap-28">
     <SwiperCarousel />
+    <div class="flex items-center gap-16 px-24">
+      <AboutUsTiles v-for="(item, index) in 3" :key="`tile-${index}`" />
+    </div>
   </main>
 </template>
 
 <script setup lang="ts">
-import { SwiperCarousel } from '../components';
+import { SwiperCarousel, AboutUsTiles } from '../components';
 </script>
