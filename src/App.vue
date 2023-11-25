40bba577e76dd1fe6d67d1dd0955aa87dd6739a8
Alen Ichshanov <a.ichshanov@adamar.solutions>
2023-11-26 04:33:42 +0600

    chore: shared

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,10 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { RouterView, useRoute } from 'vue-router'
-import { HeaderComponent, FooterComponent } from './components'
+import { HeaderComponent, FooterComponent } from '@/shared'
 
 const route = useRoute()
 
 const isFooterVisible = computed(() => route.name !== 'not-found')
 </script>
+./shared
