1fe0907af3e112f55f3e304b3b86b67714a11c84
Alen Ichshanov <a.ichshanov@adamar.solutions>
2023-11-26 17:37:03 +0600

    feat: add i18n

--- src/shared/NavLinks.vue
+++ src/shared/NavLinks.vue
@@ -1,10 +1,20 @@
 <template>
   <nav class="flex items-center gap-10 uppercase transition">
-    <RouterLink to="/about-us" class="hover:text-gray-300 hover:underline hover:duration-300">О нас</RouterLink>
-    <RouterLink to="/gallery" class="hover:text-gray-300 hover:underline hover:duration-300">Галерея</RouterLink>
-    <RouterLink to="/news/list" class="hover:text-gray-300 hover:underline hover:duration-300">Новости</RouterLink>
-    <RouterLink to="/partners" class="hover:text-gray-300 hover:underline hover:duration-300">Партнеры</RouterLink>
-    <RouterLink to="/contacts" class="hover:text-gray-300 hover:underline hover:duration-300">Контакты</RouterLink>
+    <RouterLink to="/about-us" class="hover:text-gray-300 hover:underline hover:duration-300">{{
+      $t('navLinks.about')
+    }}</RouterLink>
+    <RouterLink to="/gallery" class="hover:text-gray-300 hover:underline hover:duration-300">{{
+      $t('navLinks.gallery')
+    }}</RouterLink>
+    <RouterLink to="/news/list" class="hover:text-gray-300 hover:underline hover:duration-300">{{
+      $t('navLinks.news')
+    }}</RouterLink>
+    <RouterLink to="/partners" class="hover:text-gray-300 hover:underline hover:duration-300">{{
+      $t('navLinks.partners')
+    }}</RouterLink>
+    <RouterLink to="/contacts" class="hover:text-gray-300 hover:underline hover:duration-300">{{
+      $t('navLinks.contacts')
+    }}</RouterLink>
   </nav>
 </template>
 
