7351e911acce8419a853b3cd4ee8a3bb5c309381
alen.ichshanov <alen.ichshanov@chocotravel.com>
2024-02-02 01:50:36 +0600

    feat: add partners links

---
+++ src/pages/home/config/index.ts
@@ -0,0 +1,62 @@
+export const SHOP_PARTNERS = [
+  {
+    img: 'partner-1.png',
+    link: 'https://www.gien.com/'
+  },
+  {
+    img: 'partner-2.png',
+    link: 'https://us.peugeot-saveurs.com'
+  },
+  {
+    img: 'partner-3.svg',
+    link: 'https://www.jules-pansu.com/en/'
+  },
+  {
+    img: 'partner-4.png',
+    link: 'https://larochere-na.com'
+  },
+  {
+    img: 'partner-5.png',
+    link: 'https://goodwill.be/en/home-eng/'
+  },
+  {
+    img: 'partner-6.png',
+    link: 'https://www.ivvnet.it/en/home-2/'
+  },
+  {
+    img: 'partner-7.png',
+    link: 'https://elvangdenmark.com'
+  },
+  {
+    img: 'partner-8.png',
+    link: 'https://www.goebel.de/en/'
+  },
+  {
+    img: 'partner-9.png',
+    link: 'https://tokyo-design-studio.com/en/'
+  },
+  {
+    img: 'partner-10.png',
+    link: 'http://www.cowparade.com'
+  },
+  {
+    img: 'partner-11.png',
+    link: 'https://www.aulica.fr/fr/'
+  },
+  {
+    img: 'partner-12.png',
+    link: 'https://www.burleigh.co.uk'
+  },
+  {
+    img: 'partner-13.png',
+    link: 'https://authenticmodels.com'
+  },
+  {
+    img: 'partner-14.svg',
+    link: 'https://lacornue.com/en-GB'
+  },
+  {
+    img: 'partner-15.png',
+    link: 'https://www.amclassicfurniture.com/amclassic/'
+  }
+]
---
+++ src/pages/home/ui/components/PartnerCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <a :href="partner.link" target="_blank">
+    <img :src="partnerImg" alt="" width="100" height="70" />
+  </a>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import type { PartnerData } from '../../model'
+
+const props = defineProps<{
+  partner: PartnerData
+}>()
+
+const partnerImg = computed(() => {
+  const path = new URL('../../../../../public/partners', import.meta.url).href
+  return `${path}/${props.partner.img}`
+})
+</script>
