c81a85fddcd5e4ce0f3df66e654ca6ce0e939d3e
alen.ichshanov <alen.ichshanov@chocotravel.com>
2024-02-16 03:36:36 +0600

    fix(mobile): adjust about us page styles

--- src/pages/about-us/ui/AboutUsPage.vue
+++ src/pages/about-us/ui/AboutUsPage.vue
@@ -1,17 +1,18 @@
 <template>
-  <main class="flex flex-col items-center py-[100px]">
-    <div class="absolute top-0 left-0 h-[1000px] w-full bg-green-100 -z-10"></div>
+  <main class="flex flex-col items-center py-12 lg:py-[100px]">
+    <div class="absolute top-0 left-0 h-[500px] lg:h-[1000px] w-full bg-green-100 -z-10"></div>
     <div class="flex flex-col items-center gap-12 mb-[120px]">
-      <h1 class="font-cormorant font-bold text-[60px] uppercase text-center">{{ $t('aboutUs.title') }}</h1>
-      <p class="w-4/5 font-cormorant font-bold text-[64px] text-center">
+      <h1 class="font-cormorant font-bold text-3xl lg:text-[60px] uppercase text-center">{{ $t('aboutUs.title') }}</h1>
+      <p class="w-4/5 font-cormorant font-bold text-2xl lg:text-[64px] text-center">
         {{ $t('aboutUs.desc') }}
       </p>
       <img src="@/assets/about-main.jpg" alt="" />
     </div>
-    <img src="@/assets/rivoli-logo-3.svg" width="400" alt="" class="mb-12" />
-    <div class="flex flex-col gap-5 w-[475px] pl-8">
+    <img src="@/assets/rivoli-logo-3.svg" width="400" alt="" class="mb-12 hidden lg:block" />
+    <img src="@/assets/rivoli-logo-3.svg" width="200" alt="" class="mb-12 lg:hidden" />
+    <div class="flex flex-col gap-5 lg:w-[475px] pl-8">
       <p class="font-montserrat text-xl font-medium uppercase">{{ $t('aboutUs.help.title') }}</p>
-      <div class="flex flex-col text-gray-400 font-montserrat">
+      <div class="flex flex-col gap-3 lg:gap-0 text-gray-400 font-montserrat">
         <p>• {{ $t('aboutUs.help.create') }}</p>
         <p>• {{ $t('aboutUs.help.items') }}</p>
         <p>• {{ $t('aboutUs.help.order') }}</p>
@@ -19,7 +20,7 @@
     </div>
     <OurPartners class="w-full" />
     <img src="@/assets/rivoli-gallery.jpg" width="570" height="684" alt="" class="mt-[120px]" />
-    <p class="mt-24 w-[360px] font-cormorant text-[64px] font-bold text-center leading-[70px]">
+    <p class="mt-24 w-[360px] font-cormorant text-3xl lg:text-[64px] font-bold text-center leading-[70px]">
       {{ $t('aboutUs.check') }}
     </p>
     <PageLink :route="{ name: 'gallery' }" :text="$t('pageLink.gallery')" has-margin />
