740299f0a04ba8a96d22a8fd37eeeb5cdb63e73f
alen.ichshanov <alen.ichshanov@chocotravel.com>
2024-02-18 16:58:26 +0600

    fix: fix gallery block carousel

--- src/pages/home/ui/components/GalleryBlock.vue
+++ src/pages/home/ui/components/GalleryBlock.vue
@@ -2,7 +2,20 @@
   <div class="relative flex flex-col items-center pt-16">
     <img src="@/assets/logo-back.svg" class="absolute top-0 -right-[200px]" alt="" />
     <h4 class="mb-10 text-[50px] uppercase font-semibold font-cormorant">{{ $t('home.gallery') }}</h4>
-    <GalleryCarousel />
+    <div class="hidden lg:grid grid-cols-5 items-center gap-8 px-16 mt-[120px]">
+      <div class="flex flex-col items-center gap-6 col-span-2">
+        <div class="flex flex-col gap-4">
+          <img src="@/assets/pre-gallery-1.png" width="384" height="586" alt="" />
+        </div>
+        <div class="flex flex-col gap-4">
+          <img src="@/assets/pre-gallery-2.png" width="384" height="586" alt="" />
+        </div>
+      </div>
+      <div class="flex flex-col gap-4 col-span-3">
+        <img src="@/assets/pre-gallery-3.png" width="716" height="716" alt="" />
+      </div>
+    </div>
+    <GalleryCarousel class="lg:hidden" />
     <PageLink :route="{ name: 'gallery' }" :text="$t('pageLink.gallery')" has-margin />
   </div>
 </template>
