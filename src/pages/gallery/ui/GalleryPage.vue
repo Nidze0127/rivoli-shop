255ebbf2e81f405e1b96f3886c51a05ebb2eab40
alen.ichshanov <alen.ichshanov@chocotravel.com>
2024-03-16 02:19:11 +0600

    fix: gallery img

--- src/pages/gallery/ui/GalleryPage.vue
+++ src/pages/gallery/ui/GalleryPage.vue
@@ -1,11 +1,11 @@
 <template>
   <main class="flex flex-col items-center gap-10 lg:gap-[90px] py-12 lg:py-[100px]">
-    <div class="flex flex-col items-center font-cormorant">
+    <div class="flex flex-col gap-6 items-center font-cormorant">
       <h1 class="font-bold text-3xl lg:text-[60px] uppercase text-center">{{ $t('gallery.title') }}</h1>
-      <p class="font-bold text-lg lg:text-xl">{{ $t('gallery.photos') }}</p>
+      <p class="font-bold text-lg uppercase">{{ $t('gallery.photos') }}</p>
     </div>
     <div class="flex flex-col lg:grid lg:grid-cols-2 gap-10">
-      <img v-for="item in 36" :key="`gallery-photo-${item}`" :src="photoSrc(item)" alt="" />
+      <img v-for="item in 33" :key="`gallery-photo-${item}`" :src="photoSrc(item)" alt="" />
     </div>
   </main>
 </template>
