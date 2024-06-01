407d92afdcb7a08d16f5b5640462adc72acf5c3a
alen.ichshanov <alen.ichshanov@chocotravel.com>
2024-06-01 20:37:24 +0500

    fix: main title en

--- src/pages/home/ui/components/AboutUs.vue
+++ src/pages/home/ui/components/AboutUs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hidden lg:flex flex-col gap-40">
     <div class="relative w-[1000px] self-center mt-[90px]">
-      <div v-if="locale !== 'kz'" class="flex flex-wrap items-end gap-x-10 pl-[100px] xl:pl-0">
+      <div v-if="locale === 'ru'" class="flex flex-wrap items-end gap-x-10 pl-[100px] xl:pl-0">
         <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[120px] text-green-200 uppercase leading-[120px]">
           {{ $t('home.about.gallery') }}
         </p>
@@ -15,7 +15,7 @@
           {{ $t('home.about.outstanding') }}
         </p>
       </div>
-      <div v-if="locale === 'kz'" class="flex flex-wrap items-end gap-x-10 pl-[100px] xl:pl-0">
+      <div v-else-if="locale === 'kz'" class="flex flex-wrap items-end gap-x-10 pl-[100px] xl:pl-0">
         <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[110px] text-green-200 uppercase leading-[120px]">
           ЖАЙЛЫЛЫҚ ПЕН
         </p>
@@ -23,6 +23,26 @@
         <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[100px] uppercase leading-[120px]">ГАЛЕРЕЯСЫ</p>
         <p class="w-[220px] xl:w-[300px] mb-5 ml-8 text-[13px] xl:text-lg font-light font-montserrat text-right">
           Әлемге әйгілі өндірушілердің ерекше жиһаздары мен интерьерлері
+        </p>
+      </div>
+      <div v-else-if="locale === 'en'" class="flex flex-wrap items-end gap-x-10 pl-[100px] xl:pl-0">
+        <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[110px] text-green-200 uppercase leading-[120px]">
+          Gallery
+        </p>
+        <p
+          class="w-fit ml-32 font-cormorant font-bold text-[80px] xl:text-[110px] text-green-200 uppercase leading-[120px]"
+        >
+          of
+        </p>
+        <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[100px] uppercase leading-[120px]">Coziness</p>
+        <p
+          class="w-fit ml-12 font-cormorant font-bold text-[80px] xl:text-[110px] text-green-200 uppercase leading-[120px]"
+        >
+          and
+        </p>
+        <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[100px] uppercase leading-[120px]">comfort</p>
+        <p class="w-[220px] xl:w-[300px] mb-5 -ml-10 text-[13px] xl:text-base font-light font-montserrat text-right">
+          Exceptional furniture and interior items from famous world manufacturers
         </p>
       </div>
       <ImageCarousel />
