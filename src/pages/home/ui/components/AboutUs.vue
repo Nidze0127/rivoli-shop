466a511c2abbc5b12979a86c00146094626b603f
alen.ichshanov <alen.ichshanov@chocotravel.com>
2024-06-01 20:27:59 +0500

    fix: main title kz

--- src/pages/home/ui/components/AboutUs.vue
+++ src/pages/home/ui/components/AboutUs.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hidden lg:flex flex-col gap-40">
     <div class="relative w-[1000px] self-center mt-[90px]">
-      <div class="flex flex-wrap items-end gap-x-10 pl-[100px] xl:pl-0">
+      <div v-if="locale !== 'kz'" class="flex flex-wrap items-end gap-x-10 pl-[100px] xl:pl-0">
         <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[120px] text-green-200 uppercase leading-[120px]">
           {{ $t('home.about.gallery') }}
         </p>
@@ -13,6 +13,16 @@
         </p>
         <p class="w-[220px] xl:w-[300px] mb-5 text-[13px] xl:text-base font-light font-montserrat text-right">
           {{ $t('home.about.outstanding') }}
+        </p>
+      </div>
+      <div v-if="locale === 'kz'" class="flex flex-wrap items-end gap-x-10 pl-[100px] xl:pl-0">
+        <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[110px] text-green-200 uppercase leading-[120px]">
+          ЖАЙЛЫЛЫҚ ПЕН
+        </p>
+        <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[100px] uppercase leading-[120px]">ЫҢҒАЙЛЫЛЫҚ</p>
+        <p class="w-fit font-cormorant font-bold text-[80px] xl:text-[100px] uppercase leading-[120px]">ГАЛЕРЕЯСЫ</p>
+        <p class="w-[220px] xl:w-[300px] mb-5 ml-8 text-[13px] xl:text-lg font-light font-montserrat text-right">
+          Әлемге әйгілі өндірушілердің ерекше жиһаздары мен интерьерлері
         </p>
       </div>
       <ImageCarousel />
@@ -60,6 +70,9 @@
 </template>
 
 <script setup lang="ts">
+import { useI18n } from 'vue-i18n'
 import { PageLink } from '@/shared'
 import ImageCarousel from './ImageCarousel.vue'
+
+const { locale } = useI18n()
 </script>
