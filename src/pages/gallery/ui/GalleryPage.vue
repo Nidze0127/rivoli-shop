67d65c4d6e38e10e9dd6d6e3e1a564a59c9fa641
alen.ichshanov <alen.ichshanov@chocotravel.com>
2024-06-01 20:10:48 +0500

    fix: gallery page text

--- src/pages/gallery/ui/GalleryPage.vue
+++ src/pages/gallery/ui/GalleryPage.vue
@@ -2,7 +2,7 @@
   <main class="flex flex-col items-center gap-10 lg:gap-[90px] py-12 lg:py-[100px]">
     <div class="flex flex-col gap-6 items-center font-cormorant">
       <h1 class="font-bold text-3xl lg:text-[60px] uppercase text-center">{{ $t('gallery.title') }}</h1>
-      <p class="font-bold text-lg uppercase">{{ $t('gallery.photos') }}</p>
+      <p class="font-bold uppercase">{{ $t('gallery.photos') }}</p>
     </div>
     <div class="flex flex-col lg:grid lg:grid-cols-2 gap-10">
       <img v-for="item in 33" :key="`gallery-photo-${item}`" :src="photoSrc(item)" alt="" />
